059a6c5ffbaa2fc6347be307000c4ee355fa708b
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-01-20 10:24:58 +0100

    1

---
+++ 1_podstawy.py
@@ -0,0 +1,21 @@
+print('Dzien dobry')
+
+x = 5
+y = 7.5
+suma = x+y
+print('suma wynosi')   # wyswietl slowo "suma"
+print(suma)            # wyswietl zmienna "suma"
+
+imie = 'Kamil'
+print('imie')
+print(imie)
+
+print(type(x))
+print(type(y))
+print(type(imie))
+
+suma = x * imie  # (mnożenie zmiennej całkowitej i tekstu)
+print(suma)
+
+
+
