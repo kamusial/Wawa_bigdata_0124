d75ba66eeb019072a4aa69d14d4c558b48624de2
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-01-21 11:35:07 +0100

    1

--- 3_diabetes.py
+++ 3_diabetes.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np     # bibliteka do obliczeń numerycznych
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix
 
 df = pd.read_csv('diabetes.csv')
@@ -21,29 +24,62 @@ print(df.isna().sum())    #sprawdź
 
 df.to_csv('cukrzyca.csv', sep=';', index=False)
 
-print('regresja logistyczna')
 X = df.iloc[ : , :-1 ]    # dane wejściowe, bez ceny i bez id
 y = df.outcome              # dane wyjściowe
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)   # dane testowe 20%
 
+print('\nregresja logistyczna')
 model = LogisticRegression()
 model.fit(X_train, y_train)    #naucz się na danych treningowych
 print(model.score(X_test, y_test))   #sprawdź
 print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
 
-print('\nZmiana danych')
+print('\nKNN')
+model = KNeighborsClassifier()
+model.fit(X_train, y_train)    #naucz się na danych treningowych
+print(model.score(X_test, y_test))   #sprawdź
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\nTree')
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)    #naucz się na danych treningowych
+print(model.score(X_test, y_test))   #sprawdź
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\nSVC')
+model = SVC()
+model.fit(X_train, y_train)    #naucz się na danych treningowych
+print(model.score(X_test, y_test))   #sprawdź
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\n\n\nZmiana danych')
 df1 = df.query('outcome==0').sample(n=500)    # wybierz 500 zdrowych
 df2 = df.query('outcome==1').sample(n=500)    # wybierz 500 chorych
 df3 = pd.concat([df1, df2])     # łączenie tabel
-print(df3)
 
 X = df3.iloc[ : , :-1 ]    # dane wejściowe, bez ceny i bez id
 y = df3.outcome              # dane wyjściowe
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)   # dane testowe 20%
-
+print('\nregresja logistyczna')
 model = LogisticRegression()
+model.fit(X_train, y_train)    #naucz się na danych treningowych
+print(model.score(X_test, y_test))   #sprawdź
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\nKNN')
+model = KNeighborsClassifier()
+model.fit(X_train, y_train)    #naucz się na danych treningowych
+print(model.score(X_test, y_test))   #sprawdź
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\nTree')
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)    #naucz się na danych treningowych
+print(model.score(X_test, y_test))   #sprawdź
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\nSVC')
+model = SVC()
 model.fit(X_train, y_train)    #naucz się na danych treningowych
 print(model.score(X_test, y_test))   #sprawdź
 print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
