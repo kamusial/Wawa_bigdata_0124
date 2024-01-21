b230fe618a7460c1adfbd7c0dff388b4ebcb8001
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-01-21 11:22:28 +0100

    1

--- 3_diabetes.py
+++ 3_diabetes.py
@@ -29,5 +29,21 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)   # dan
 
 model = LogisticRegression()
 model.fit(X_train, y_train)    #naucz się na danych treningowych
-print(model.score(X_test, y_test))
+print(model.score(X_test, y_test))   #sprawdź
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\nZmiana danych')
+df1 = df.query('outcome==0').sample(n=500)    # wybierz 500 zdrowych
+df2 = df.query('outcome==1').sample(n=500)    # wybierz 500 chorych
+df3 = pd.concat([df1, df2])     # łączenie tabel
+print(df3)
+
+X = df3.iloc[ : , :-1 ]    # dane wejściowe, bez ceny i bez id
+y = df3.outcome              # dane wyjściowe
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)   # dane testowe 20%
+
+model = LogisticRegression()
+model.fit(X_train, y_train)    #naucz się na danych treningowych
+print(model.score(X_test, y_test))   #sprawdź
 print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
